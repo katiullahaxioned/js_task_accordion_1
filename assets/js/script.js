88d7684d5b724a954b531bda475e7bfb980cb706
katiullahaxioned <atiullahk@axioned>
2022-11-01 11:07:52 +0530

    functionality optimised

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,16 @@
 var openCloseBox = document.querySelectorAll(".open-close-box");
-var Open = document.querySelectorAll(".open");
-var Close = document.querySelectorAll(".close");
-var list = document.querySelectorAll(".list-items li");
-var heading = document.querySelectorAll(".list-items h3");
-var para = document.querySelectorAll(".list-items p");
+var list = document.querySelectorAll(".item");
 
-// **************** click functionality start **************** //
-function initialOpenCall(initialElement) {
-  initialElement.classList.add("active");
-}
+list[0].classList.add("active");
 
-initialOpenCall(Open[0]);
-initialOpenCall(Close[0]);
-initialOpenCall(list[0]);
-initialOpenCall(heading[0]);
-initialOpenCall(para[0]);
-
-function commonOpen(boxIndex, element, elementIndex) {
-  if (boxIndex === elementIndex) element.classList.add("active");
-}
-
-function forEachOpenClose(boxIndex, elements) {
-  elements.forEach(function (element, elementIndex) {
-    element.classList.remove("active");
-    commonOpen(boxIndex, element, elementIndex);
-  });
-}
-
-function openClose(boxIndex) {
-  forEachOpenClose(boxIndex, Open);
-  forEachOpenClose(boxIndex, Close);
-  forEachOpenClose(boxIndex, list);
-  forEachOpenClose(boxIndex, heading);
-  forEachOpenClose(boxIndex, para);
-}
-// ***************** click functionality end ***************** //
-
-// ************* double click functionality start ************* //
-function forEachClose(elements) {
-  elements.forEach(function (element) {
-    element.classList.remove("active");
-  })
-}
-
-function dblClickClose() {
-  forEachClose(Open);
-  forEachClose(Close);
-  forEachClose(list);
-  forEachClose(heading);
-  forEachClose(para);
-}
-// ************* double click functionality end ************* //
-
-openCloseBox.forEach(function (box, boxIndex) {
+openCloseBox.forEach(function (box, ind) {
   box.addEventListener("click", function () {
-    openClose(boxIndex);
-  });
+    for (let i = 0; i < list.length; i++) {
+      if (i != ind) {
+        list[i].classList.remove("active");
+      }
+    }
 
-  box.addEventListener("dblclick", function () {
-    dblClickClose();
+    list[ind].classList.toggle("active");
   });
 });
